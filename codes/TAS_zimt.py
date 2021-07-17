Fixing calculation of impedance

codes/TAS_zimt.py
@@ -34,13 +34,12 @@ def TAS():
 
     Vdc = 1
     Vamp = 10e-3
-    num_probe =  1
+    num_probe =  10
     num_periods = 4
     Gen = 0
     tVG_path = path2ZimT
     
-
-    fprobe_span = np.logspace(1,1, num_probe) 
+    fprobe_span = np.logspace(1,6, num_probe) 
 
     str_lst, sys_lst, path_lst, tVG_lst, tj_lst = [], [], [], [], []
 
@@ -65,10 +64,10 @@ def TAS():
         path_lst.append(path2ZimT)
         tVG_lst.append(tVG_name)
         tj_lst.append(tj_name)
-    # p = multiprocessing.Pool(max_jobs)
-    # results = parmap.starmap(run_zimt, list(zip(str_lst,sys_lst,path_lst)), pm_pool=p, pm_processes=max_jobs,pm_pbar=True)
-    # p.close()
-    # p.join()
+    p = multiprocessing.Pool(max_jobs)
+    results = parmap.starmap(run_zimt, list(zip(str_lst,sys_lst,path_lst)), pm_pool=p, pm_processes=max_jobs,pm_pbar=True)
+    p.close()
+    p.join()
 
     if move_output: 
         store_output_in_folder(tVG_lst,store_folder,path2ZimT)
@@ -101,11 +100,13 @@ def TAS():
     realZ = [Z.real for Z in tZ]
     imagZ = [-Z.imag for Z in tZ]
 
-    plt.plot(fprobe_span, imagZ)
-    plt.xscale('log')
-    plt.show()
 
-    plt.plot(realZ, imagZ )
-    plt.show
+    # print(tZ)
+    # plt.plot(fprobe_span, imagZ)
+    # plt.xscale('log')
+    # plt.show()
+
+    # plt.plot(realZ, imagZ )
+    # plt.show
 if __name__ == "__main__":
     TAS()

codes/VLC_useful_func.py
@@ -1065,24 +1065,28 @@ def store_output_in_folder(filenames,folder_name,path):
 
 def calcZC(tV, tJ, freq, equalCircuit):
 
-    tV_fft = fftpack.fft(tV)[1:int(len(tV)/2)]
+    tV_fft = fftpack.fft(tV)/len(tV)
+    tV_fft = tV_fft[0:round(len(tV)/2)]
+    tV_fft[0] = 0
+    tJ_fft = fftpack.fft(tJ)/len(tJ)
+    tJ_fft = tJ_fft[0:round(len(tJ)/2)]
+    tJ_fft[0] = 0
 
-    tJ_fft = fftpack.fft(tJ)[1:int(len(tV)/2)]
     Z = max(tV_fft)/max(tJ_fft)
-   
 
     if equalCircuit == 'RCp':
         C = 1/(2*np.pi*freq) * (1/Z).imag 
     elif equalCircuit == 'RCs' :
         C = -1/(2*np.pi*freq*Z.imag) 
     
-    print(tJ)
+    plt.plot(tV)
+    plt.show()
 
-    # plt.stem(abs(tV_fft))
-    # plt.show()
+    plt.stem(abs(tV_fft))
+    plt.show()
 
-    # plt.stem(abs(tJ_fft))
-    # plt.show()
+    plt.stem(abs(tJ_fft))
+    plt.show()
 
     return Z, C
 