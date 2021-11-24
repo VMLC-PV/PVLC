f0a3f631c538e0aab24496177e41c433400845b4
Vincent M. Le Corre <vincent.m.le.corre@gmail.com>
2021-11-24 21:44:39 +0100

    restructuring
    retructuring useful vlc function

---
+++ codes/VLC_units/__init__.py
@@ -0,0 +1,3 @@
+import VLC_units.plots.plot_settings_screen
+from VLC_units.plots.SimSS_plots import *
+from VLC_units.run_simu.runsim import *
---
+++ codes/VLC_units/run_simu/runsim.py
@@ -0,0 +1,99 @@
+######################################################################
+############### Function to run simulation code ######################
+######################################################################
+# by Vincent M. Le Corre
+# Package import
+import numpy as np
+import pandas as pd
+import matplotlib as mp
+import matplotlib.pyplot as plt
+from matplotlib.legend import Legend
+from scipy import stats,optimize,constants
+import subprocess,shutil,os,glob,tqdm,parmap,multiprocessing,random,sys,platform
+from itertools import repeat
+import warnings
+import sys
+# Don't show warnings
+warnings.filterwarnings("ignore")
+
+
+def run_code(name_prog,path2prog,str2run='',show_term_output=False):
+    """Run program 'name_prog' in the folder specified by 'path2prog'.
+
+    Parameters
+    ----------
+    name_prog : str
+        name of the program to run.
+
+    st2run : str
+        String to run for the name_prog.
+    
+    System : str
+        String to specify the operating system.
+        The options are: 'Linux' or 'Windows'
+    
+    path2prog : str
+        path to the folder containing zimt 
+        ('./zimt' in Linux and 'zimt.exe' in Windows ).
+
+    Returns
+    -------
+    
+    """
+    
+    System = platform.system()                  # Operating system
+    is_windows = (System == 'Windows')          # Check if we are on Windows
+    path2prog = str(path2prog)                  # Convert to string
+    curr_dir = os.getcwd()                      # Get current directory
+    os.chdir(path2prog)                         # Change directory to working directory
+
+    if show_term_output == True:
+        output_direct = None
+    else:
+        output_direct = subprocess.DEVNULL
+    
+    if is_windows:
+        cmd_list = name_prog.lower()+'.exe ' + str2run
+    else : 
+        cmd_list = './'+name_prog.lower()+' ' + str2run
+    try:
+        subprocess.check_call(cmd_list.split(), encoding='utf8', stdout=output_direct, cwd=path2prog, shell=is_windows)
+    except subprocess.CalledProcessError:
+        print(path2prog)
+        raise ChildProcessError
+    os.chdir(curr_dir)                          # Change directory back to original directory
+
+def run_multiprocess_simu(prog2run,code_name_lst,path_lst,str_lst,max_jobs=max(1,os.cpu_count()-1)):
+    """run_multiprocess_simu runs simulations in parrallel (if possible) on max_jobs number of threads
+
+    Parameters
+    ----------
+    prog2run : function
+        name of the function that runs the simulations
+
+    code_name_lst : list of str
+        list of names of the codes to run
+    
+    str_lst : list of str
+        List containing the strings to run for the simulations
+
+    path_lst : list of str
+        List containing the path to the folder containing the simulation program
+    
+    max_jobs : int, optional
+        Number of threads used to run the simulations, by default os.cpu_count()-1
+    """
+    p = multiprocessing.Pool(max_jobs)
+    results = parmap.starmap(prog2run,list(zip(code_name_lst,path_lst,str_lst)), pm_pool=p, pm_processes=max_jobs,pm_pbar=True)
+    p.close()
+    p.join()
+
+if __name__ == '__main__':
+    
+    System = platform.system()                  # Operating system
+    is_windows = (System == 'Windows')          # Check if we are on Windows
+    
+    if is_windows:
+        run_code('SimSS','c:/Users/lecor/Desktop/GitHub/PVLC/codes/Simulation_program/SIMsalabim_v425/SimSS',str2run='-L 110e-9')
+    else:
+        run_code('SimSS','/mnt/c/Users/lecor/Desktop/GitHub/PVLC/codes/Simulation_program/SIMsalabim_v425/SimSS',str2run='-L 110e-9')
