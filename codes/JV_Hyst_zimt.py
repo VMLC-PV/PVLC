3f042e0848b0f8730782bb451a8263b6796bbe86
Vincent M. Le Corre <vincent.m.le.corre@gmail.com>
2021-12-02 23:09:50 +0100

    JV Hyst update

--- codes/JV_Hyst_zimt.py
+++ codes/JV_Hyst_zimt.py
@@ -3,81 +3,104 @@
 ###################################################
 # by Vincent M. Le Corre
 # Package import
-import os,sys,platform,tqdm,parmap,multiprocessing,warnings
+import os,sys,platform,warnings
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
-from matplotlib.lines import Line2D
-from scipy.integrate import simps
-from scipy.optimize import curve_fit
 from scipy import constants
 from time import time
-from itertools import repeat
-# Don't show warnings
-warnings.filterwarnings("ignore")
-# Homemade package import
-import plot_settings_screen
-from VLC_useful_func import * #sci_notation,run_zimt,zimt_tj_plot,zimt_tj_JV_plot,Store_output_in_folder,clean_up_output
-from tVG_gen import zimt_JV_double_sweep
-q = constants.value(u'elementary charge')
+# Import homemade package by VLC
+from VLC_units.plots.ZimT_plots import *
+from VLC_units.simu.runsim import *
+from VLC_units.simu.get_input_par import *
+from VLC_units.make_tVG.tVG_gen import *
+from VLC_units.cleanup_folder.clean_folder import *
+from VLC_units.useful_functions.aux_func import *
+
 # Main Program
-def main():    
-    # General Inputs
-    warnings.filterwarnings("ignore")   # Don't show warnings
+def JV_Hyst(fixed_str = None, input_dic = None, path2ZimT = None, run_simu = True, plot_tjs = True, move_ouput_2_folder = True, Store_folder = 'JV_Hyst',clean_output = False,verbose = True):  
+    """Run single JV sweep simulation using ZimT
+
+    Parameters
+    ----------
+    fixed_str : str, optional
+        Add any fixed string to the simulation command for zimt, by default None.
+    input_dic : dict, optional
+        Dictionary with the input for the zimt_JV_Hyst function (see tVG_gen.py), by default None.
+    path2ZimT : str, optional
+        Path to ZimT, by default None
+    run_simu : bool, optional
+        Rerun simu?, by default True
+    plot_tjs : bool, optional
+        make plot ?, by default True
+    move_ouput_2_folder : bool, optional
+        Move output to folder?, by default True
+    Store_folder : str, optional
+        Folder name for storing output, by default 'JV_Hyst'
+    clean_output : bool, optional
+        Clean up output?, by default False
+    verbose : bool, optional
+        Verbose?, by default True
+    """      
+
+    ## General Inputs
+    warnings.filterwarnings("ignore")           # Don't show warnings
     system = platform.system()                  # Operating system
-    max_jobs = os.cpu_count()-2                       # Max number of parallel simulations (for number of CPU use: os.cpu_count() )
-    if system == 'Windows':             # cannot easily do multiprocessing in Windows
+    max_jobs = os.cpu_count()-2                 # Max number of parallel simulations (for number of CPU use: os.cpu_count() )
+    do_multiprocessing = True                      # Use multiprocessing
+    if system == 'Windows':                     # cannot easily do multiprocessing in Windows
             max_jobs = 1
-            slash = '/'
-            try:
+            do_multiprocessing = False
+            try:                                # kill all running jobs to avoid conflicts
                 os.system('taskkill.exe /F /IM zimt.exe')
             except:
                 pass
-    else:
-        slash = '/'
 
     curr_dir = os.getcwd()              # Current working directory
-    path2ZimT = 'Simulation_program/DDSuite_v405_pero/ZimT'+slash    # Path to ZimT in curr_dir
+    if path2ZimT is None:
+        path2ZimT = os.path.join(os.getcwd(),'Simulation_program/SIMsalabim_v425/ZimT')                  # Path to ZimT in curr_dir
 
-    # Physics constants
+ 
+    ## Physics constants
     q = constants.value(u'elementary charge')
 
-    # Simulation input
-    run_simu = 0                                         # Rerun simu?
-    plot_tjs = True                                        # make plot ?
-    plot_output = False
-    move_ouput_2_folder = True
-    Store_folder = 'JV_hysteresis'+slash
-    clean_output = False
-    L = 440e-9                                                  # Device thickness (m)
-    Gens = [215/(q*400e-9)]                                               # Max generation rate for the gaussian laser pulse
-    print(Gens)
-    # scans = np.geomspace(1e-3,5e3,num=10)
-    
-    Vstart = 1.17
-    Vfinal = 0
+    # TPC input
+    # see zimt_Hyst in tVG_gen.py
+    if input_dic is None:
+        if verbose:
+            print('No JV_Hyst input dictionary given, using default values')
+        Vstart = 1.17                                               # Start voltage (unit: V)
+        Vfinal = 0                                                  # Final voltage (unit: V)
+        scans = np.geomspace(1e-3, 1e4,num=10)                                               # Scan speed (unit: V/s)
+        Gens = [3.7e27]                                             # Average generation rate (unit: m^-3 s^-1) 
+        steps = 100                                                 # Number of voltage step
+    else:
+        if 'Vstart' in input_dic:
+            Vstart = input_dic['Vstart']
+        if 'Vfinal' in input_dic:
+            Vfinal = input_dic['Vfinal']
+        if 'scans' in input_dic:
+            scans = input_dic['scans']
+        if 'Gens' in input_dic:
+            Gens = input_dic['Gens']
+        if 'steps' in input_dic:
+            steps = input_dic['steps']
 
-    # scans_exp = [0.01,0.06,0.2,1,2,10,20,100,200,1000,2000]
-    # rev_exp = [18.8,19,19.07694,19.15601,19.45013,19.72537,19.81781,19.809,20.20233,20.35866,20.1]
-    # for_exp = [18.8,18.8,18.2,17.71846,17.601,17.25488,17.69805,18.618,19.22702,19.85594,19.98172]
+    ## Prepare strings to run
+    # Fixed string
+    if fixed_str is None:
+        if verbose:
+            print('No fixed string given, using default value')
+        fixed_str = ' -accDens 0.05'  # add any fixed string to the simulation command
 
-    # scans_exp = np.asarray([0.00569,0.0285,0.057,0.286,0.572,2.86,5.72,28.6,57.2,286,572])*4
-    # for_exp = [18.59206,17.85167,17.67393,17.33705,17.1059,16.58675,16.54971,17.85843,18.6046,19.73997,19.78995]
-    # rev_exp = [18.31429,18.32017,18.61573,19.11934,19.3399,19.70609,19.86326,20.03486,20.06419,20.23221,20.44356]
-    # data_exp = pd.read_csv(curr_dir+slash+'summary_Fast_hysteresis_eff.dat',sep=',')
-    # print(data_exp)
-    # scans_exp = data_exp['speed_mean']*4
-    # for_exp = data_exp['PCE_for_mean']
-    # rev_exp = data_exp['PCE_rev_mean']
-    data_exp = pd.read_csv(curr_dir+slash+'405_pxA_effs.dat',sep=',')
-    print(data_exp)
-    scans_exp = data_exp['speed_mean']*4
-    scans = data_exp['speed_mean']*4
-    for_exp = data_exp['PCE_for_mean']
-    rev_exp = data_exp['PCE_rev_mean']
     # Initialize 
-    sys_lst,str_lst,path_lst,tj_lst,tVG_lst = [],[],[],[],[]
+    str_lst,code_name_lst,path_lst,tj_lst,tVG_lst = [],[],[],[],[]
+    idx = 0
+    start = time()
+
+    # Initialize 
+    code_name_lst,str_lst,path_lst,tj_lst,tVG_lst = [],[],[],[],[]
     idx = 0
     start = time()
 
@@ -91,28 +114,27 @@ def main():
     # Generate tVG files and str_lst 
         for scan in scans:
             for Gen in Gens:
-                zimt_JV_double_sweep(Vstart,Vfinal,scan,Gen,100,tVG_name=curr_dir+slash+path2ZimT+'tVG_JV_Hyst_scan_{:.2e}_G_{:.2e}.txt'.format(scan,Gen))
-                str_lst.append('-L '+str(L)+' -NP 2000 -tVG_file tVG_JV_Hyst_scan_{:.2e}_G_{:.2e}.txt -tj_file tj_JV_Hyst_scan_{:.2e}_G_{:.2e}.dat'.format(scan,Gen,scan,Gen))
-                sys_lst.append(system)
-                path_lst.append(curr_dir+slash+path2ZimT)
+                zimt_JV_double_sweep(Vstart,Vfinal,scan,Gen,steps,tVG_name=os.path.join(path2ZimT,'tVG_JV_Hyst_scan_{:.2e}_G_{:.2e}.txt'.format(scan,Gen)))
+                str_lst.append(fixed_str+' -tVG_file tVG_JV_Hyst_scan_{:.2e}_G_{:.2e}.txt -tj_file tj_JV_Hyst_scan_{:.2e}_G_{:.2e}.dat'.format(scan,Gen,scan,Gen))
+                code_name_lst.append('zimt')
+                path_lst.append(path2ZimT)
                 tVG_lst.append('tVG_JV_Hyst_scan_{:.2e}_G_{:.2e}.txt'.format(scan,Gen))
                 tj_lst.append('tj_JV_Hyst_scan_{:.2e}_G_{:.2e}.dat'.format(scan,Gen))
 
-        # print(str_lst)
         # Run ZimT
-        # str_lst = str_lst[::-1] # reverse list order to start with longest delays
-        p = multiprocessing.Pool(max_jobs)
-        results = parmap.starmap(run_zimt,list(zip(str_lst,sys_lst,path_lst)), pm_pool=p, pm_processes=max_jobs,pm_pbar=True)
-        p.close()
-        p.join()
+        if do_multiprocessing:
+            run_multiprocess_simu(run_code,code_name_lst,path_lst,str_lst,max_jobs)
+        else:
+            for i in range(len(str_lst)):
+                run_code(code_name_lst[i],path_lst[i],str_lst[i],show_term_output=True,verbose=verbose)
          
     print('Calculation time {:.2f} s'.format(time() - start)) # Time in seconds
 
     
     ## Move output folder to new folder
     if move_ouput_2_folder: # Move outputs to Store_folder
-        Store_output_in_folder(tVG_lst,Store_folder,curr_dir+slash+path2ZimT)
-        Store_output_in_folder(tj_lst,Store_folder,curr_dir+slash+path2ZimT)
+        Store_output_in_folder(tVG_lst,Store_folder,path2ZimT)
+        Store_output_in_folder(tj_lst,Store_folder,path2ZimT)
 
     
     ########################################################
@@ -124,7 +146,7 @@ def main():
         Voc_dir2,Jsc_dir2,FF_dir2,PCE_dir2 = [],[],[],[]
         for scan in scans:
             for Gen in Gens:
-                    data_tj = pd.read_csv(curr_dir+slash+path2ZimT+Store_folder+'tj_JV_Hyst_scan_{:.2e}_G_{:.2e}.dat'.format(scan,Gen),delim_whitespace=True)
+                    data_tj = pd.read_csv(os.path.join(path2ZimT,Store_folder,'tj_JV_Hyst_scan_{:.2e}_G_{:.2e}.dat'.format(scan,Gen)),delim_whitespace=True)
                     
                     # Seperate scan directions
                     # abs(data_Var.Vext -Vext) == min(abs(data_Var.Vext -Vext))
@@ -147,16 +169,14 @@ def main():
                     PCE_dir2.append(get_PCE(scan_dir2['Vext'],scan_dir2['Jext'])/10)
                     
 
-                    zimt_tj_JV_plot(0,scan_dir1,x='Vext',y=['Jext'],xlimits = [-0.1,1.25],ylimits=[-22,2] ,colors=colors[idx],labels=sci_notation(scan, sig_fig=1)+' V s$^{-1}$',pic_save_name=curr_dir+slash+path2ZimT+Store_folder+'Fast_hyst_JV.jpg',legend=False)
-                    zimt_tj_JV_plot(0,scan_dir2,x='Vext',y=['Jext'],xlimits = [-0.1,1.25],ylimits=[-22,2] ,colors=colors[idx],line_type=['--'],labels=sci_notation(scan, sig_fig=1)+' V s$^{-1}$',pic_save_name=curr_dir+slash+path2ZimT+Store_folder+'Fast_hyst_JV.jpg',legend=False)
+                    zimt_tj_JV_plot(0,scan_dir1,x='Vext',y=['Jext'],xlimits = [-0.1,1.25],ylimits=[-22,2] ,colors=colors[idx],labels=sci_notation(scan, sig_fig=1)+' V s$^{-1}$',pic_save_name=os.path.join(path2ZimT,Store_folder,'Fast_hyst_JV.jpg'),legend=False)
+                    zimt_tj_JV_plot(0,scan_dir2,x='Vext',y=['Jext'],xlimits = [-0.1,1.25],ylimits=[-22,2] ,colors=colors[idx],line_type=['--'],labels=sci_notation(scan, sig_fig=1)+' V s$^{-1}$',pic_save_name=os.path.join(path2ZimT,Store_folder,'Fast_hyst_JV.jpg'),legend=False)
                     idx = idx + 1 
 
         plt.figure(1)
         plt.subplot(221)
         plt.semilogx(scans,PCE_dir2,linestyle='-',marker='None',markersize=10,markerfacecolor='w',label='Forward')
         plt.semilogx(scans,PCE_dir1,linestyle='-',marker='None',markersize=10,markerfacecolor='w',label='Reverse')
-        plt.semilogx(scans_exp,for_exp,linestyle='None',marker='o',markersize=10,markerfacecolor='w',label='Forward exp')
-        plt.semilogx(scans_exp,rev_exp,linestyle='None',marker='^',markersize=10,markerfacecolor='w',label='Reverse exp')
         plt.xlabel('Scan speed [V s$^{-1}$]')
         plt.ylabel('PCE [%]')
         plt.ylim([10,25])
@@ -164,8 +184,6 @@ def main():
         plt.subplot(222)
         plt.semilogx(scans,FF_dir2,linestyle='-',marker='o',markersize=10,markerfacecolor='w',label='Forward')
         plt.semilogx(scans,FF_dir1,linestyle='-',marker='^',markersize=10,markerfacecolor='w',label='Reverse')
-        plt.semilogx(scans_exp,data_exp['FF_for_mean'],linestyle='None',marker='o',markersize=10,markerfacecolor='w',label='Forward exp')
-        plt.semilogx(scans_exp,data_exp['FF_rev_mean'],linestyle='None',marker='^',markersize=10,markerfacecolor='w',label='Reverse exp')
         plt.xlabel('Scan speed [V s$^{-1}$]')
         plt.ylabel('FF')
         plt.ylim([0.5,1])
@@ -173,8 +191,6 @@ def main():
         plt.subplot(223)
         plt.semilogx(scans,Voc_dir2,linestyle='-',marker='o',markersize=10,markerfacecolor='w',label='Forward')
         plt.semilogx(scans,Voc_dir1,linestyle='-',marker='^',markersize=10,markerfacecolor='w',label='Reverse')
-        plt.semilogx(scans_exp,data_exp['Voc_for_mean'],linestyle='None',marker='o',markersize=10,markerfacecolor='w',label='Forward exp')
-        plt.semilogx(scans_exp,data_exp['Voc_rev_mean'],linestyle='None',marker='^',markersize=10,markerfacecolor='w',label='Reverse exp')
         plt.xlabel('Scan speed [V s$^{-1}$]')
         plt.ylabel('V$_{OC}$ [V]')
         plt.ylim([0.9,1.3])
@@ -182,31 +198,26 @@ def main():
         plt.subplot(224)
         plt.semilogx(scans,abs(np.asarray(Jsc_dir2)),linestyle='-',marker='o',markersize=10,markerfacecolor='w',label='Forward')
         plt.semilogx(scans,abs(np.asarray(Jsc_dir1)),linestyle='-',marker='^',markersize=10,markerfacecolor='w',label='Reverse')
-        plt.semilogx(scans_exp,data_exp['Jsc_for_mean'],linestyle='None',marker='o',markersize=10,markerfacecolor='w',label='Forward exp')
-        plt.semilogx(scans_exp,data_exp['Jsc_rev_mean'],linestyle='None',marker='^',markersize=10,markerfacecolor='w',label='Reverse exp')
         plt.xlabel('Scan speed [V s$^{-1}$]')
         plt.ylabel('J$_{SC}$ mA cm$^{-2}$')
         plt.ylim([18,26])
         # plt.legend(loc='best', title="Scan direction:",fontsize=35)
         plt.tight_layout()
         plt.grid(b=True,which='both')
-        plt.savefig(curr_dir+slash+path2ZimT+Store_folder+'Fast_hyst_eff.jpg',dpi=100,transparent=True)
+        plt.savefig(os.path.join(path2ZimT,Store_folder,'Fast_hyst_eff.jpg'),dpi=100,transparent=True)
 
         print(PCE_dir1[0]-PCE_dir1[-1])
         
 
     ## Clean-up outputs from folder
     if clean_output: # delete all outputs
-        clean_up_output('tj',curr_dir+slash+path2ZimT+Store_folder)
-        clean_up_output('tVG',curr_dir+slash+path2ZimT+Store_folder)
-        print('Ouput data was deleted from '+curr_dir+slash+path2ZimT+Store_folder)
+        clean_up_output('tj',os.path.join(path2ZimT,Store_folder))
+        clean_up_output('tVG',os.path.join(path2ZimT,Store_folder))
+        print('Ouput data was deleted from '+os.path.join(path2ZimT,Store_folder))
 
-    
-
-    
     print('Elapsed time {:.2f} s'.format(time() - start)) # Time in seconds
 if __name__ == '__main__':
-    main()
+    JV_Hyst()
     plt.show()
     
     
